a6efc13af190ba5154ab0e602e82cc511811cc84
isinelnikov <kingreeve@mail.ru>
2019-05-21 23:43:58 +0300

    Refactoring project.
    Update pom version: <pom.version>1.0.0-public</pom.version>.

--- src/main/java/notification/service/domain/notification/rich/RichTemplate.java
+++ src/main/java/notification/service/domain/notification/rich/RichTemplate.java
@@ -1,89 +1,26 @@
 package notification.service.domain.notification.rich;
 
-import notification.service.utils.JsonUtils;
+import notification.service.domain.notification.rich.component.RichTextElement;
 
 import java.util.List;
 import java.util.Objects;
-import javax.annotation.Nullable;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RichTemplate {
+    private final List<RichTextElement> elements;
 
-    // Common fields
-    private final String bodyHref;
-    @Nullable
-    private final String bodyImageLink;
-    private final boolean ringtoneOn;
-    @Nullable
-    private final List<Integer> vibration;
-
-    // Notification bodies
-    @Nullable
-    private final RichSmallNotification smallNotification;
-    @Nullable
-    private final RichBigNotification bigNotification;
-
-    @JsonCreator
-    public RichTemplate(@JsonProperty("href") String bodyHref,
-            @JsonProperty("top_image") @Nullable String topImageLink,
-            @JsonProperty("ringtone_on") boolean ringtoneOn,
-            @JsonProperty("vibration") @Nullable List<Integer> vibration,
-            @JsonProperty("small_notification") @Nullable RichSmallNotification smallNotification,
-            @JsonProperty("big_notification") @Nullable RichBigNotification bigNotification) {
-        this.bodyHref = Objects.requireNonNull(bodyHref, "Body href can't be null.");
-        this.ringtoneOn = ringtoneOn;
-        this.bodyImageLink = topImageLink;
-        this.vibration = vibration;
-        this.smallNotification = smallNotification;
-        this.bigNotification = bigNotification;
+    public RichTemplate(@JsonProperty("elements") List<RichTextElement> elements) {
+        this.elements = Objects.requireNonNull(elements, "Elements can't be null.");
     }
 
-    @JsonProperty("href")
-    public String getBodyHref() {
-        return bodyHref;
-    }
-
-    @Nullable
-    @JsonProperty("top_image")
-    public String getBodyImageLink() {
-        return JsonUtils.getStringTrimOrNull(bodyImageLink);
-    }
-
-    @JsonProperty("ringtone_on")
-    public boolean isRingtoneOn() {
-        return ringtoneOn;
-    }
-
-    @Nullable
-    @JsonProperty("vibration")
-    public List<Integer> getVibration() {
-        return vibration;
-    }
-
-    @Nullable
-    @JsonProperty("small_notification")
-    public RichSmallNotification getSmallNotification() {
-        return smallNotification;
-    }
-
-    @Nullable
-    @JsonProperty("big_notification")
-    public RichBigNotification getBigNotification() {
-        return bigNotification;
+    public List<RichTextElement> getElements() {
+        return elements;
     }
 
     @Override
     public String toString() {
         return "RichTemplate{" +
-                "bodyHref='" + bodyHref + '\'' +
-                ", bodyImageLink='" + bodyImageLink + '\'' +
-                ", ringtoneOn=" + ringtoneOn +
-                ", vibration=" + vibration +
-                ", smallNotification=" + smallNotification +
-                ", bigNotification=" + bigNotification +
+                "elements=" + elements +
                 '}';
     }
 }
