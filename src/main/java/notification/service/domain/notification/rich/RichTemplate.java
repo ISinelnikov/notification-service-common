5c4e8fd082d01bdba3e29e7571193ebda018e57d
isinelnikov <kingreeve@mail.ru>
2019-05-22 07:18:01 +0300

    Added new fields to rich template.

--- src/main/java/notification/service/domain/notification/rich/RichTemplate.java
+++ src/main/java/notification/service/domain/notification/rich/RichTemplate.java
@@ -8,16 +8,30 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RichTemplate {
     private final String templateId;
+    private final String templateTitle;
+    private final String internalTitle;
     private final List<RichTextElement> elements;
 
     public RichTemplate(@JsonProperty("templateId") String templateId,
+            @JsonProperty("templateTitle") String templateTitle,
+            @JsonProperty("internalTitle") String internalTitle,
             @JsonProperty("elements") List<RichTextElement> elements) {
         this.templateId = Objects.requireNonNull(templateId, "Template id can't be null.");
+        this.templateTitle = Objects.requireNonNull(templateTitle, "Template title can't be null.");
+        this.internalTitle = Objects.requireNonNull(internalTitle, "Internal title can't be null.");
         this.elements = Objects.requireNonNull(elements, "Elements can't be null.");
     }
 
     public String getTemplateId() {
         return templateId;
+    }
+
+    public String getTemplateTitle() {
+        return templateTitle;
+    }
+
+    public String getInternalTitle() {
+        return internalTitle;
     }
 
     public List<RichTextElement> getElements() {
@@ -28,6 +42,8 @@ public class RichTemplate {
     public String toString() {
         return "RichTemplate{" +
                 "templateId='" + templateId + '\'' +
+                ", templateTitle='" + templateTitle + '\'' +
+                ", internalTitle='" + internalTitle + '\'' +
                 ", elements=" + elements +
                 '}';
     }
