275a9633b7976989be7846ee0c99fcaba08c0d66
isinelnikov <kingreeve@mail.ru>
2019-05-22 06:53:34 +0300

    Added missing notification field.

--- src/main/java/notification/service/domain/notification/rich/RichTemplate.java
+++ src/main/java/notification/service/domain/notification/rich/RichTemplate.java
@@ -7,10 +7,17 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RichTemplate {
+    private final String templateId;
     private final List<RichTextElement> elements;
 
-    public RichTemplate(@JsonProperty("elements") List<RichTextElement> elements) {
+    public RichTemplate(@JsonProperty("templateId") String templateId,
+            @JsonProperty("elements") List<RichTextElement> elements) {
+        this.templateId = Objects.requireNonNull(templateId, "Template id can't be null.");
         this.elements = Objects.requireNonNull(elements, "Elements can't be null.");
+    }
+
+    public String getTemplateId() {
+        return templateId;
     }
 
     public List<RichTextElement> getElements() {
@@ -20,7 +27,8 @@ public class RichTemplate {
     @Override
     public String toString() {
         return "RichTemplate{" +
-                "elements=" + elements +
+                "templateId='" + templateId + '\'' +
+                ", elements=" + elements +
                 '}';
     }
 }
